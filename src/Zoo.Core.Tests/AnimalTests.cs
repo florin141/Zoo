9486e8e30f07f781897155c965f57f80f16d19e3
florin141 <29834996+florin141@users.noreply.github.com>
2019-11-06 19:34:27 +0100

    Add tests

---
+++ src/Zoo.Core.Tests/AnimalTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Zoo.Core.Domain;
+
+namespace Zoo.Core.Tests
+{
+    public class AnimalTests
+    {
+        [Fact]
+        public void ItExists()
+        {
+            var animal = new Animal
+            {
+                Id = 0,
+                Name = "Lion",
+                Description = "Lion description",
+                HabitatId = 1,
+                Habitat = new Habitat()
+            };
+
+            Assert.NotNull(animal);
+            Assert.NotNull(animal.Habitat);
+            Assert.Equal("Lion", animal.Name);
+            Assert.Equal(1, animal.HabitatId);
+            Assert.Equal("Lion description", animal.Description);
+        }
+    }
+}
---
+++ src/Zoo.Core.Tests/HabitatTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Zoo.Core.Domain;
+
+namespace Zoo.Core.Tests
+{
+    public class HabitatTests
+    {
+        [Fact]
+        public void ItExists()
+        {
+            var habitat = new Habitat
+            {
+                Id = 1,
+                Name = "African savanna",
+                Description = "Habitat description",
+                Animals = new List<Animal>
+                {
+                    new Animal(),
+                    new Animal()
+                }
+            };
+
+            Assert.NotNull(habitat);
+            Assert.Equal("African savanna", habitat.Name);
+            Assert.Equal("Habitat description", habitat.Description);
+
+            Assert.NotNull(habitat.Animals);
+            Assert.Equal(2, habitat.Animals.Count);
+        }
+    }
+}
