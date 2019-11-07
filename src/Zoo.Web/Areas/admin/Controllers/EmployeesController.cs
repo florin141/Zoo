5adfccf29b36d61bdd5fb86b208e547e62119b99
florin141 <29834996+florin141@users.noreply.github.com>
2019-11-08 00:23:07 +0100

    Improve deleting ux

--- src/Zoo.Web/Areas/admin/Controllers/EmployeesController.cs
+++ src/Zoo.Web/Areas/admin/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Zoo.Core.Domain;
 using Zoo.Services.Employees;
@@ -35,7 +36,7 @@ namespace Zoo.Web.Areas.admin.Controllers
             {
                 FirstName = model.FirstName,
                 LastName = model.LastName,
-                BirthDate = DateTime.Now,
+                BirthDate = model.BirthDate,
                 Picture = model.Picture.ToBase64String()
             };
 
@@ -44,7 +45,7 @@ namespace Zoo.Web.Areas.admin.Controllers
             return RedirectToAction("Index");
         }
 
-        public IActionResult Delete(int id)
+        public IActionResult Edit(int id)
         {
             var employee = _employeeService.GetEmployeeById(id);
 
@@ -53,13 +54,50 @@ namespace Zoo.Web.Areas.admin.Controllers
                 return NotFound();
             }
 
-            return View(employee);
+            var employeeDto = new EmployeeDto
+            {
+                Id = employee.Id,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                BirthDate = employee.BirthDate,
+                Picture = null
+            };
+            
+
+            return View(employeeDto);
         }
 
         [HttpPost]
-        public IActionResult Delete(Employee employee)
+        public IActionResult Edit(EmployeeDto model)
         {
-            _employeeService.DeleteEmployee(employee);
+            var employeeFromDb = _employeeService.GetEmployeeById(model.Id);
+
+            employeeFromDb.FirstName = model.FirstName;
+            employeeFromDb.LastName = model.LastName;
+            employeeFromDb.BirthDate = model.BirthDate;
+
+            if (model.Picture != null)
+            {
+                employeeFromDb.Picture = model.Picture.ToBase64String();
+            }
+
+            _employeeService.UpdateEmployee(employeeFromDb);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            // TODO Add DeleteEmployeeById to get rid of the double trip to DB
+            var employeeFromDb = _employeeService.GetEmployeeById(id);
+
+            if (employeeFromDb == null)
+            {
+                return NotFound();
+            }
+
+            _employeeService.DeleteEmployee(employeeFromDb);
 
             return RedirectToAction("Index");
         }
--- src/Zoo.Web/Areas/admin/Models/EmployeeDto.cs
+++ src/Zoo.Web/Areas/admin/Models/EmployeeDto.cs
@@ -5,6 +5,7 @@ namespace Zoo.Web.Areas.admin.Models
 {
     public class EmployeeDto
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime BirthDate { get; set; }
