c5b0c78dfe0c1a0b3554b42ce0837d856ba85a91
Florin CIOBANU <29834996+florin141@users.noreply.github.com>
2020-08-30 03:02:23 +0200

    Add AnimalDto class

---
+++ src/Zoo.Web/Areas/admin/Models/AnimalDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zoo.Web.Areas.admin.Models
+{
+    public class AnimalDto
+    {
+    }
+}
